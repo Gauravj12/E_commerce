b208414ba46e2fb7f6a5fd15e4ed5545fe926dff
git config --global user.name GauravJ1AtDure <92713528+GauravJ1AtDure@users.noreply.github.com>
2024-05-11 13:08:42 +0530

    json updated

--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
-var products
+let products
+let women_sec=document.querySelector('#women_sec')
 
 fetch("/data/products.json")
 .then(response => {
@@ -8,7 +9,59 @@ fetch("/data/products.json")
     //console.log(data)
     //dataRow1 = JSON.stringify(data[0].products)
     products = data.products
-    console.log(products)
+    let allProducts=[]
+    allProducts=[products.kids, products.men, products.women]
+    console.log(allProducts)
+
+    allProducts.map((product, index) => {
+    
+        all_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
+        <div class = "collection-img position-relative">
+            <img src = ${product.thumbnail} class = "w-100">
+            <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
+        </div>
+        <div class = "text-center">
+            <div class = "rating mt-3">
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+            </div>
+            <p class = "text-capitalize my-1">${product.title}</p>
+            <span class = "fw-bold">${product.price}</span>
+        </div>
+    </div>
+    `
+    
+    });
+
+
+
+    products.women.map((product, index) => {
+    
+        women_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
+        <div class = "collection-img position-relative">
+            <img src = ${product.thumbnail} class = "w-100">
+            <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
+        </div>
+        <div class = "text-center">
+            <div class = "rating mt-3">
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+            </div>
+            <p class = "text-capitalize my-1">${product.title}</p>
+            <span class = "fw-bold">${product.price}</span>
+        </div>
+    </div>
+    `
+    
+    });
+
+
  })
 
  
