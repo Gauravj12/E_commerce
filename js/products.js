5f3514d25e3828304d923f19a27dc70a658e51f8
Gauravj12 <gauravjadhav1205@gmail.com>
2024-05-12 19:39:50 +0530

    update product categories

--- js/products.js
+++ js/products.js
@@ -1,5 +1,16 @@
 let products
+let all_sec=document.querySelector('#all_sec')
 let women_sec=document.querySelector('#women_sec')
+let men_sec=document.querySelector('#men_sec')
+let kids_sec=document.querySelector('#kids_sec')
+let val
+
+let getVal=(e)=>{
+
+    val=e
+    console.log(e)
+}
+
 
 fetch("/data/products.json")
 .then(response => {
@@ -10,14 +21,14 @@ fetch("/data/products.json")
     //dataRow1 = JSON.stringify(data[0].products)
     products = data.products
     let allProducts=[]
-    allProducts=[products.kids, products.men, products.women]
+    allProducts=[...products.kids, ...products.men, ...products.women]
     console.log(allProducts)
 
     allProducts.map((product, index) => {
     
         all_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
         <div class = "collection-img position-relative">
-            <img src = ${product.thumbnail} class = "w-100">
+            <img src = ${product.thumbnail} class = "rounded mx-auto d-block img-fluid">
             <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
         </div>
         <div class = "text-center">
@@ -37,12 +48,59 @@ fetch("/data/products.json")
     });
 
 
-
     products.women.map((product, index) => {
     
         women_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
         <div class = "collection-img position-relative">
-            <img src = ${product.thumbnail} class = "w-100">
+            <img src = ${product.thumbnail} class = "rounded mx-auto d-block img-fluid">
+            <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
+        </div>
+        <div class = "text-center">
+            <div class = "rating mt-3">
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+            </div>
+            <p class = "text-capitalize my-1">${product.title}</p>
+            <span class = "fw-bold">${product.price}</span>
+        </div>
+    </div>
+    `
+    
+    });
+
+
+    products.men.map((product, index) => {
+    
+        men_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
+        <div class = "collection-img position-relative">
+            <img src = ${product.thumbnail} class = "rounded mx-auto d-block img-fluid">
+            <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
+        </div>
+        <div class = "text-center">
+            <div class = "rating mt-3">
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+                <span class = "text-primary"><i class = "fas fa-star"></i></span>
+            </div>
+            <p class = "text-capitalize my-1">${product.title}</p>
+            <span class = "fw-bold">${product.price}</span>
+        </div>
+    </div>
+    `
+    
+    });
+
+
+    products.kids.map((product, index) => {
+    
+        kids_sec.innerHTML += ` <div class = "col-md-6 col-lg-4 col-xl-3 p-2 best">
+        <div class = "collection-img position-relative">
+            <img src = ${product.thumbnail} class = "rounded mx-auto d-block img-fluid">
             <span class = "position-absolute bg-primary text-white d-flex align-items-center justify-content-center">sale</span>
         </div>
         <div class = "text-center">
